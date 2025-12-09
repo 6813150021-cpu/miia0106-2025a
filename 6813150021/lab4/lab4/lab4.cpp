10e807cec7539189f43b61d835efdc3ac95dd121
bb <6813150021@mut.ac.th>
2025-12-09 18:19:18 +0700

    rf

---
+++ 6813150021/lab4/lab4/lab4.cpp
@@ -0,0 +1,43 @@
+// lab4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "for" << endl;
+	for (int i = 1; i <= 10; i++) {
+	  cout << i << " ";
+	}
+	cout << "------------------------------" << endl;
+	
+	cout << "while" << endl;
+	int j = 1;
+	while (j <= 10)
+	 {
+	    cout << j << " ";
+	    j++;
+	 }
+	cout << "------------------------------" << endl;
+
+	cout << "do while" << endl;
+	int k = 1;
+	do {
+		cout << k << " ";
+		k++;
+	} while (k <= 10);
+	cout << "------------------------------" << endl;
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
